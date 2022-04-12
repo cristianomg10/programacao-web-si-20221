040ea2af855191a1c8cb636f629f5fa552ae0cbb
Lucas Scolaro <lucas.scolaro@ifsc.edu.br>
2022-04-11 22:47:36 -0300

    Aula 11/4

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -5,8 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Endereco;
+
 class Cliente extends Model
 {
     use HasFactory;
     protected $table = "clientes";
+
+    function enderecos(){
+        return $this->hasMany(Endereco::class, 'id_cliente', 'id');
+    }
 }
