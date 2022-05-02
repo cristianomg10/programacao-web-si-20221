f45af306a0a2feca20baa4a9d793d64dd4d6079d
Lucas Scolaro <lucas.scolaro@ifsc.edu.br>
2022-05-02 19:52:41 -0300

    02/05

--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,13 @@ Route::get('/clientes/listar', [ClientesController::class, 'listar'])->name('cli
 Route::middleware('auth')->group(function () {
     Route::get('/clientes/novo', [ClientesController::class, 'cadastro_novo']);
     Route::post('/clientes/novo', [ClientesController::class, 'novo'])->name('clientes_novo');
-    Route::get('/clientes/alterar/{id}', [ClientesController::class, 'alterar'])->name('clientes_alterar');
-    Route::post('/clientes/alterar/', [ClientesController::class, 'salvar'])->name('clientes_salvar');
-    Route::get('/clientes/excluir/{id}', [ClientesController::class, 'excluir'])->name('clientes_excluir');
+    
+    Route::middleware('verifica.nivel')->group(function(){
+        Route::get('/clientes/alterar/{id}', [ClientesController::class, 'alterar'])->name('clientes_alterar');
+        Route::post('/clientes/alterar/', [ClientesController::class, 'salvar'])->name('clientes_salvar');
+        Route::get('/clientes/excluir/{id}', [ClientesController::class, 'excluir'])->name('clientes_excluir');
+    });
+    
 });
 
 require __DIR__.'/auth.php';
